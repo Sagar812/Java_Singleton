3e0e8f8ad7fc1075d9417ad65892a5c5600655f1
Sagar Parkale <parkale.sagar8@gmail.com>
2018-12-15 14:36:49 +0530

    Create singleton class
    >We can create only one object of singleton class
    >In above programe we uses static function to get reference of singleton class object

---
+++ Singleton_Demo.java
@@ -0,0 +1,28 @@
+import java.lang.*;
+
+class Singleton_Demo
+{
+    private static Singleton_Demo obj = new Singleton_Demo();
+    /*
+    obj is static, so that its memory gets created at the time of class loading
+    and its memory gets shared
+    */
+    
+    private Singleton_Demo()    //contructor is private so that we canot call it from outside of this class
+    {
+        System.out.println("Constructor of class");
+    }
+    
+    public static Singleton_Demo getReference()   //this method of class is also static, so that we can call it by using class name
+    {
+        return Singleton_Demo.obj;    //this will return the 'obj'
+    }
+}
+
+class Test
+{
+    public static void main(String[] args)
+    {
+        Singleton_Demo ref = Singleton_Demo.getreference();
+    }
+}
